ed67811792f84e2096855d1c2376594d29398c37
Suuny <71173328+ZXTHR@users.noreply.github.com>
2020-11-23 17:04:51 +0800

    Update TextAreaLayer.java

--- src/TextAreaLayer.java
+++ src/TextAreaLayer.java
@@ -1,18 +1,29 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Date;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
 
 public class TextAreaLayer extends JPanel {
 	
-	JTextArea jTextArea;
-	
+	JTextArea Time;
+	Date now;
+	SimpleDateFormat formatter;
 
 	public TextAreaLayer() {
 		String names = "Student Name and ID: Ku Wing Fung (18075712d) \n"
-				+ "Student Name and ID: Wong Tsz Hin (18050573d) \n";
-		
-		jTextArea = new JTextArea(names);
+				+ "Student Name and ID: Wong Tsz Hin (18050573d)";
+		now = new Date(System.currentTimeMillis());
+		formatter = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
+		Time = new JTextArea(formatter.format(now));
 		
 		setLayout(new GridLayout(0,1));
-		add(jTextArea);
+		add(new JTextArea(names));
+		add(Time);
+	}
+
+	public void refreshTime() {
+		now = new Date(System.currentTimeMillis());
+		Time.setText(formatter.format(now));
 	}
 }
